a07b51e9d3b9ee2babc64447c8e6b8046f8626fe
Robert Brothers <rbrothers@chipscan.us>
2023-08-05 09:45:25 -0400

    ADD-0001: Added project 1: blink LEDs in a pattern

--- project1/verilog/project1.v
+++ project1/verilog/project1.v
@@ -4,7 +4,7 @@ module button_counter(
 	input clk,	// this is the 12 MHz clock
 
 	// outputs (reg retain value until reassignment)
-	output reg [4:0] led
+	output reg [7:0] led
 );
 
 	reg [23:0] timer;
@@ -13,14 +13,14 @@ module button_counter(
 	always @ (posedge clk)
 	begin
 		timer <= timer + 1'b1;
-		if ( timer == 24'd1000000 )
+		if ( timer == 24'd10000000 )
 		begin
 			led[timer2] <= led[timer2] ^ 1'b1;// shift_value;
 			led[timer2-1] <= 1;// shift_value;
 			timer2 <= timer2 + 1;
 			timer <= 0;
 
-			if ( timer2 == 3'd5 )
+			if ( timer2 == 4'd8 )
 			begin
 				timer2 <= 0;
 			end
